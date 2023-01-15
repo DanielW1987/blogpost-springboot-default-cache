ce3a53cba67c19291267db8ef1841f6394608a94
Daniel Wagner <wagner.daniel87@gmail.com>
2023-01-15 18:30:23 +0100

    use cache manager also for area of circle and multiply cache

--- src/main/java/rocks/danielw/cache/CacheConfig.java
+++ src/main/java/rocks/danielw/cache/CacheConfig.java
@@ -15,10 +15,12 @@ import java.util.Arrays;
 public class CacheConfig {
 
   public static final String STUDENTS_CACHE_NAME = "studentsCache";
+  public static final String AREA_OF_CIRCLE_CACHE_NAME = "areaOfCircleCache";
+  public static final String MULTIPLY_CACHE_NAME = "multiplyCache";
 
   @Bean
   public CacheManager cacheManager() {
-    return new ConcurrentMapCacheManager(STUDENTS_CACHE_NAME);
+    return new ConcurrentMapCacheManager(STUDENTS_CACHE_NAME, AREA_OF_CIRCLE_CACHE_NAME, MULTIPLY_CACHE_NAME);
   }
 
   @Bean
