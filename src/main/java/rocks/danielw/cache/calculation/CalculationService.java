4ade0557339c7d7ac2738200135f108168c56430
Daniel Wagner <wagner.daniel87@gmail.com>
2023-01-22 16:08:20 +0100

    use constant cache names

--- src/main/java/rocks/danielw/cache/calculation/CalculationService.java
+++ src/main/java/rocks/danielw/cache/calculation/CalculationService.java
@@ -5,23 +5,26 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import static rocks.danielw.cache.CacheConfig.AREA_OF_CIRCLE_CACHE_NAME;
+import static rocks.danielw.cache.CacheConfig.MULTIPLY_CACHE_NAME;
+
 @Service
 @Slf4j
 public class CalculationService {
 
-  @Cacheable(value = "areaOfCircleCache", key = "#radius", condition = "#radius > 5")
+  @Cacheable(value = AREA_OF_CIRCLE_CACHE_NAME, key = "#radius", condition = "#radius > 5")
   public double areaOfCircle(int radius) {
     log.info("calculate the area of a circle with a radius of {}", radius);
     return Math.PI * Math.pow(radius, 2);
   }
 
-  @Cacheable(value = "multiplyCache", keyGenerator = "multiplyKeyGenerator")
+  @Cacheable(value = MULTIPLY_CACHE_NAME, keyGenerator = "multiplyKeyGenerator")
   public double multiply(int factor1, int factor2) {
     log.info("Multiply {} with {}", factor1, factor2);
     return factor1 * factor2;
   }
 
-  @CacheEvict(cacheNames = {"areaOfCircleCache", "multiplyCache"}, allEntries = true)
+  @CacheEvict(cacheNames = {AREA_OF_CIRCLE_CACHE_NAME, MULTIPLY_CACHE_NAME}, allEntries = true)
   public void evictCache() {
     log.info("Evict all cache entries...");
   }
